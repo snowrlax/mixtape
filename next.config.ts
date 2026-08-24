1ec012bdeacc731d7952e03a79fac1e825582e58
snowrlax <sonawanepranav19@gmail.com>
2026-08-24 21:09:07 +0530

    feat: update next.js

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
-  webpack(config: any) {
-    config.module.rules.push({
-      test: /\.mp3$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-      },
-    });
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
